b7ab7196a198e01e30a84ed71c88813db66723f8
Tim Jones <tim@timjones.io>
2018-11-06 22:13:22 +0000

    Remove unused code
    Syntax tagging is now done by a TextMate grammar definition

--- src/server/ShaderTools.CodeAnalysis.ShaderLab.Workspaces/Classification/ShaderLabClassificationService.cs
+++ src/server/ShaderTools.CodeAnalysis.ShaderLab.Workspaces/Classification/ShaderLabClassificationService.cs
@@ -5,9 +5,7 @@ using System.Composition;
 using System.Threading;
 using ShaderTools.CodeAnalysis.Classification;
 using ShaderTools.CodeAnalysis.Compilation;
-using ShaderTools.CodeAnalysis.ShaderLab.Syntax;
 using ShaderTools.CodeAnalysis.Host.Mef;
-using ShaderTools.CodeAnalysis.Syntax;
 using ShaderTools.CodeAnalysis.Text;
 
 namespace ShaderTools.CodeAnalysis.ShaderLab.Classification
@@ -15,27 +13,6 @@ namespace ShaderTools.CodeAnalysis.ShaderLab.Classification
     [ExportLanguageService(typeof(IClassificationService), LanguageNames.ShaderLab), Shared]
     internal class ShaderLabClassificationService : AbstractClassificationService
     {
-        //public override IEnumerable<ISyntaxClassifier> GetDefaultSyntaxClassifiers()
-        //{
-        //    return SyntaxClassifier.DefaultSyntaxClassifiers;
-        //}
-
-        //public override void AddLexicalClassifications(SourceText text, TextSpan textSpan, List<ClassifiedSpan> result, CancellationToken cancellationToken)
-        //{
-        //    ClassificationHelpers.AddLexicalClassifications(text, textSpan, result, cancellationToken);
-        //}
-
-        public override void AddSyntacticClassifications(SyntaxTreeBase syntaxTree, TextSpan textSpan, List<ClassifiedSpan> result, CancellationToken cancellationToken)
-        {
-            var worker = new SyntaxTaggerWorker(result, cancellationToken);
-            worker.ClassifySyntax((SyntaxTree) syntaxTree);
-        }
-
-        //public override ClassifiedSpan FixClassification(SourceText rawText, ClassifiedSpan classifiedSpan)
-        //{
-        //    return ClassificationHelpers.AdjustStaleClassification(rawText, classifiedSpan);
-        //}
-
         public override void AddSemanticClassifications(SemanticModelBase semanticModel, TextSpan textSpan, Workspace workspace, List<ClassifiedSpan> result, CancellationToken cancellationToken)
         {
             // TODO
